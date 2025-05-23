644b513f49b9a1b929a37529008156aee76b9aa9
Adam <adam.conkle@gmail.com>
2025-05-23 15:41:12 -0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "todo-app-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
+];
+
+// Install event
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
